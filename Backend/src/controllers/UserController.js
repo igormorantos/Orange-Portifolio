9ea3cae1d51158270282ee1a0ff9d70a0658c7d7
Igor Moreira <igor.moreira@lucastechnologyservice.com>
2024-01-24 20:37:09 -0300

    logica da rota login

--- Backend/src/controllers/UserController.js
+++ Backend/src/controllers/UserController.js
@@ -1,8 +1,7 @@
 const cnn = require('../repository/connection')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
-const senhaJwt = require('../services/secretkey')
-
+const secretkey = require('../services/secretkey')
 
 const addUser = async (req, res) => {
     try{
@@ -28,6 +27,32 @@ const addUser = async (req, res) => {
     }
 }
 
+const login = async (req, res) =>{
+    try{
+        const { email, password } = req.body
+        const user = await cnn.query('SELECT * FROM user WHERE email = :email', {
+            replacements: { email },
+            type: cnn.QueryTypes.SELECT
+        });
+        const passwordValid = await bcrypt.compare(password, user[0].password);
+    
+        if(user.length == 0 || !passwordValid){
+            return res.status(404).json({ mensagem: 'Email ou senha invalida' });
+        }
 
+        const token =jwt.sign({ id: user[0].id }, secretkey, { expiresIn: '8h' });
+        const{password: _, ...loggedUser} = user[0];
+        
+        return res.json({ usuario: loggedUser, token });
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(500).json({ mensagem: 'Erro interno do servidor' })
+    }
+}
 
-module.exports = addUser
+module.exports = {
+    addUser,
+    login
+}
+//669308203280-5ja80sk7gmn29dpk6b0n091bpf4q8ar4.apps.googleusercontent.com
