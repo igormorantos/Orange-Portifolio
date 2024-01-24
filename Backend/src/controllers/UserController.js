c39c86664afc417e49eab2bacb1baaabad03bb57
Igor Moreira <igor.moreira@lucastechnologyservice.com>
2024-01-24 09:43:49 -0300

    correção de identação e estrutura

--- Backend/src/controllers/UserController.js
+++ Backend/src/controllers/UserController.js
@@ -5,22 +5,29 @@ const senhaJwt = require('../services/secretkey')
 
 
 const addUser = async (req, res) => {
-    const { firstName, lastName, email, password} = req.body
+    try{
+        const { firstName, lastName, email, password} = req.body
 
-    const salt = await bcrypt.genSalt(10);
-    const criptpass = await bcrypt.hash(password, salt);
-    const newUser = await cnn.query('INSERT INTO user (firstName, lastName, email, password) VALUES (:firstName, :lastName, :email, :password)', {
-        replacements: { firstName, lastName, email, password: criptpass },
-        type: cnn.QueryTypes.INSERT
-    });
+        const salt = await bcrypt.genSalt(10);
+        const criptpass = await bcrypt.hash(password, salt);
+        const newUser = await cnn.query('INSERT INTO user (firstName, lastName, email, password) VALUES (:firstName, :lastName, :email, :password)', {
+            replacements: { firstName, lastName, email, password: criptpass },
+            type: cnn.QueryTypes.INSERT
+        });
 
-    const user = await cnn.query('SELECT * FROM user WHERE email = :email', {
+        const user = await cnn.query('SELECT * FROM user WHERE email = :email', {
         replacements: { email },
         type: cnn.QueryTypes.SELECT
-    });
+        });
 
-    res.status(201).json(user)
+        return res.status(201).json(user)
+    }
+    catch(error){
+        console.log(error);
+        return res.status(500).json({ mensagem: 'Erro interno do servidor' })
+    }
 }
+
 
 
 module.exports = addUser
