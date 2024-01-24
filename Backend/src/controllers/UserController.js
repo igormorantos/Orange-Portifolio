44a8cbbb8a0928fcef01502f988ab524dbaa90ae
Igor Moreira <igor.moreira@lucastechnologyservice.com>
2024-01-24 08:53:58 -0300

    controller de cadastro de usuario

--- Backend/src/controllers/UserController.js
+++ Backend/src/controllers/UserController.js
@@ -1,14 +1,27 @@
-const pool = require('../repository/connection')
+const cnn = require('../repository/connection')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const senhaJwt = require('../services/secretkey')
 
 
+const addUser = async (req, res) => {
+    const { name, email, password, country } = req.body
 
-const login = async (req, res) => {
-    res.send("teste de rotas")
+    const salt = await bcrypt.genSalt(10);
+    const criptpass = await bcrypt.hash(password, salt);
+    
+    const newUser = await cnn.query('INSERT INTO user (name, email, password, country) VALUES (:name, :email, :password, :country)', {
+        replacements: { name, email, password: criptpass, country },
+        type: cnn.QueryTypes.INSERT
+    });
+
+    const user = await cnn.query('SELECT * FROM user WHERE email = :email', {
+        replacements: { email },
+        type: cnn.QueryTypes.SELECT
+    });
+
+    res.status(201).json(user)
 }
 
-const addProject = async (req, res) => {
-    res.send("teste 2")
-}
 
-
-module.exports = login, addProject
+module.exports = addUser
