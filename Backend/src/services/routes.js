991977d3ac1853eee6b8595c71535f9587b5e238
Igor Moreira <igor.moreira@lucastechnologyservice.com>
2024-01-25 09:46:23 -0300

    inserção da rota de detalhar usuario

--- Backend/src/services/routes.js
+++ Backend/src/services/routes.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const routes = express();
 const conn = require('../repository/connection');
-const {addUser, login} = require('../controllers/UserController');
+const {addUser, login, detailUser} = require('../controllers/UserController');
 const checkUserLog = require('../middlewares/validations');
 
 //User
 routes.post('/add', addUser)
-
 routes.get('/login', login)
+routes.get('/detailUser', checkUserLog, detailUser)
 
 
 module.exports = routes
