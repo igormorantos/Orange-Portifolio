4e9fd586b829e9c30fe6bf998ded6ba2410c669f
Luana Paes <luanafranca598@gmail.com>
2024-01-25 14:14:39 -0300

    Adiciona funcionalidade de alternar ícones no botão de visualização de senha

--- Frontend/pages/js/scripts.js
+++ Frontend/pages/js/scripts.js
@@ -4,6 +4,7 @@ const email = document.getElementById('email');
 const password = document.querySelector('#password');
 const validCadastro = document.querySelector('#valid-cadastro');
 const togglePasswordButton = document.getElementById('togglePassword');
+const eyeIcon = togglePasswordButton.querySelector("i");
 
 // expressão regular para validar o e-mail
 let emailStandard = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -22,6 +23,16 @@ btnValidar.addEventListener('click', function () {
 // função para exibir e esconder a senha
 togglePasswordButton.addEventListener('click', function () {
     password.type = password.type === 'password' ? 'text' : 'password';
+
+    // remove as classes
+    eyeIcon.classList.remove("fas", "fa-eye", "fa-eye-slash");
+
+    
+    if (password.type === 'password') {
+        eyeIcon.classList.add("fas", "fa-eye"); // senha oculta
+    } else {
+        eyeIcon.classList.add("fas", "fa-eye-slash"); // senha visível
+    }
 });
 
 
