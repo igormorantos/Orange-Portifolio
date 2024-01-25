15efe6da345bff6fb588df39383f3a563a3b2047
Igor Moreira <igor.moreira@lucastechnologyservice.com>
2024-01-25 08:41:32 -0300

    atualização de rotas

--- Backend/src/services/routes.js
+++ Backend/src/services/routes.js
@@ -2,10 +2,12 @@ const express = require('express');
 const routes = express();
 const conn = require('../repository/connection');
 const {addUser, login} = require('../controllers/UserController');
+const checkUserLog = require('../middlewares/validations');
 
 //User
 routes.post('/add', addUser)
 
 routes.get('/login', login)
+
 
 module.exports = routes
