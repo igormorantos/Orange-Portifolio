9ba43eb508f7f82661bb61efdbc63be45b2c7765
Luana Paes <luanafranca598@gmail.com>
2024-01-25 16:10:55 -0300

    feat(cadastro): adicionar ícones de olho no input password e implementar método POST
    Adiciona ícones de olho ao campo de senha na tela de cadastro, proporcionando aos usuários a opção de visualizar a senha enquanto a digitam. Além disso, implementa o método POST para enviar os dados do formulário ao servidor.

--- Frontend/pages/js/scripts.js
+++ Frontend/pages/js/scripts.js
@@ -1,24 +1,12 @@
 // variáveis/const do HTML
-const btnValidar = document.getElementById('cadastrar');
-const email = document.getElementById('email');
-const password = document.querySelector('#password');
 const validCadastro = document.querySelector('#valid-cadastro');
+
+const btnValidar = document.getElementById('cadastrar');
 const togglePasswordButton = document.getElementById('togglePassword');
 const eyeIcon = togglePasswordButton.querySelector("i");
 
 // expressão regular para validar o e-mail
 let emailStandard = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-//função que exibe a mensagem de sucesso - deve enviar o formulário
-btnValidar.addEventListener('click', function () {
-    let emailValue = email.value;
-
-    if (emailStandard.test(emailValue) && password.value.length >= 8) {
-        validCadastro.style.display = "flex";
-    } else {
-        validCadastro.style.display = "none";
-    }
-});
 
 // função para exibir e esconder a senha
 togglePasswordButton.addEventListener('click', function () {
@@ -27,12 +15,62 @@ togglePasswordButton.addEventListener('click', function () {
     // remove as classes
     eyeIcon.classList.remove("fas", "fa-eye", "fa-eye-slash");
 
-    
     if (password.type === 'password') {
         eyeIcon.classList.add("fas", "fa-eye"); // senha oculta
     } else {
         eyeIcon.classList.add("fas", "fa-eye-slash"); // senha visível
     }
 });
+
+// POST
+const form = document.getElementById('form-api');
+const url = 'https://reqres.in/api/users'; // fake para teste
+
+form.addEventListener('submit', evento => {
+    evento.preventDefault();
+
+    var name = document.getElementById('name').value;
+    var lastname = document.getElementById('lastname').value;
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+
+
+    if (emailStandard.test(email) && password.length >= 8) {
+        validCadastro.style.display = "flex";
+    }
+    else if (!emailStandard.test(email) || password.length < 8 || name === '' || lastname === '') {
+        const icon = validCadastro.querySelector('i');
+        const message = validCadastro.querySelector('p');
+
+        validCadastro.style.display = "flex";
+        validCadastro.style.backgroundColor = '#ee483ca5';
+        icon.style.display = 'none';
+        message.innerText = 'Revise os campos e tente novamente.';
+        message.style.color = '#222244';
+
+
+        // aguarda 1s para recarregar a página
+        setTimeout(function () {
+            location.reload();
+        }, 1000); // 1s
+        return false; // não envia o form
+    }
+
+    const valuesInputs = {
+        nameUser: name,
+        lastnameUser: lastname,
+        emailUser: email,
+        passwordUser: password
+    }
+
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(valuesInputs)
+    }).then(res => res.json()).then(data => console.log(data))
+
+})
 
 
