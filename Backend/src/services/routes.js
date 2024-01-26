6ff174b2e1fe256f2d373372ddb18607834be856
Igor Moreira <igor.moreira@lucastechnologyservice.com>
2024-01-26 07:53:32 -0300

    mudando formatação das rotas

--- Backend/src/services/routes.js
+++ Backend/src/services/routes.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const routes = express();
 const conn = require('../repository/connection');
-const {addUser, login, detailUser} = require('../controllers/UserController');
-const checkUserLog = require('../middlewares/validations');
+const {addUser, login, detailUser, editUser} = require('../controllers/UserController');
+const checkUserLogg = require('../middlewares/validations');
 
 //User
 routes.post('/add', addUser)
 routes.get('/login', login)
-routes.get('/detailUser', checkUserLog, detailUser)
+routes.patch('/editUser', checkUserLogg, editUser)
+routes.get('/detailUser', checkUserLogg, detailUser)
 
 
 module.exports = routes
