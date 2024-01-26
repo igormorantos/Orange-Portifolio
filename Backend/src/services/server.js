a49eac65296b7cb90029b91f899bae660beacbf2
Igor Moreira <igor.moreira@lucastechnologyservice.com>
2024-01-25 21:20:06 -0300

    inserção do Cors

--- Backend/src/services/server.js
+++ Backend/src/services/server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const routes = require('./routes')
+const cors = require('cors')
 
 const app = express();
 
+app.use(cors({
+    origin: 'http://127.0.0.1:5500'
+  }));
+  
 app.use(express.json());
 app.use(routes)
 
