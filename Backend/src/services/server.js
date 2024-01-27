59dcc886fb126d02ee1710f5d2a98f2cd1ab9382
Igor Moreira <igor.moreira@lucastechnologyservice.com>
2024-01-27 20:25:22 -0300

    inserção do middler do cors

--- Backend/src/services/server.js
+++ Backend/src/services/server.js
@@ -1,11 +1,12 @@
 const dotenv = require('dotenv');
 const express = require('express');
 const routes = require('./routes');
+const cors = require('cors')
 
 dotenv.config();
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use(routes)
 
