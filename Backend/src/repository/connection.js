b9090b1fc74745ac72914befa3d2f0666f4edacd
Igor Moreira <igor.moreira@lucastechnologyservice.com>
2024-01-27 13:06:19 -0300

    escondendo os dados sensiveis do banco de dados

--- Backend/src/repository/connection.js
+++ Backend/src/repository/connection.js
@@ -1,7 +1,16 @@
 const sequelize = require('sequelize');
-const conn = new sequelize("orangedb", "root", "admin123", {
-    host: 'localhost',
-    dialect: 'mysql'
+const dotenv = require('dotenv');
+dotenv.config();
+
+const passDB = process.env.PASS
+const userDB = process.env.USER
+const nameDB = process.env.NAMEDB
+const host = process.env.HOST
+const db = process.env.DIALECT
+
+const conn = new sequelize(nameDB, userDB, passDB, {
+    host: host,
+    dialect: db
 })
 
 conn.authenticate()
