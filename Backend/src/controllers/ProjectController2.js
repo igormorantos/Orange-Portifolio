b6dcc5aa3c424c2f0ebc5550749379c771d00324
igums <igormsantos1@icloud.com>
2024-02-05 15:36:44 -0300

    corrigindo rotas de projeto

---
+++ Backend/src/controllers/ProjectController2.js
@@ -0,0 +1,29 @@
+const cnn = require('../repository/connection')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const dotenv = require('dotenv');
+dotenv.config();
+
+
+const addProject = async (req, res) => {
+    try{
+        const { title, link, tags, description, coverphoto, fk_iduser} = req.body
+        
+        if(!title || !link || !tags || !description){
+            return res.status(401).json({ mensagem: "Todos os dados precisam ser preenchidos para criar projeto!" });
+        }
+
+        const newUser = await cnn.query('INSERT INTO project (title, link, tags, description, coverphoto, fk_iduser) VALUES (:title, :link, :tags, :description, :coverphoto, :fk_iduser)', {
+            replacements: { title, link, tags, description, coverphoto, fk_iduser },
+            type: cnn.QueryTypes.INSERT
+        });
+
+        return res.status(201).json({newUser, mensagem: "usuario criado"})
+    }
+    catch(error){
+        console.log(error);
+        return res.status(500).send(error)
+    }
+}
+
+module.exports = addProject
