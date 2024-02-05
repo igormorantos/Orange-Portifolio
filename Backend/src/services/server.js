c3f869356f450a898116bf4b425e747581eb4505
igums <igormsantos1@icloud.com>
2024-02-05 19:16:32 -0300

    correção do card de projetos

--- Backend/src/services/server.js
+++ Backend/src/services/server.js
@@ -9,8 +9,12 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-    origin: '*',
+    origin: '*', // Allow requests from all origins (adjust as needed)
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allow specified methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allow specified headers
+  preflightContinue: true, // Handle OPTIONS preflight requests
 }));
+
 
 app.use(express.json());
 app.use(routes)
