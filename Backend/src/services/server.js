2b6c2bb2187a97da66afbd600682b64fb20ee3f7
igums <igormsantos1@icloud.com>
2024-02-04 22:45:40 -0300

    correção da rota de deleção

--- Backend/src/services/server.js
+++ Backend/src/services/server.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-    origin: 'https://orange-portifolio-frontend.vercel.app',
+    origin: '*' //'https://orange-portifolio-frontend.vercel.app',
 }));
 
 app.use(express.json());
